4e7a3e5067828892e8fcf356537c9ff929c5edb8
Ethan <ebud6805@gmail.com>
2024-01-02 19:43:15 -0800

    Added Script Controller with Post Request

---
+++ src/main/java/net/crusadergames/bugwars/controller/ScriptController.java
@@ -0,0 +1,34 @@
+package net.crusadergames.bugwars.controller;
+
+import net.crusadergames.bugwars.model.Script;
+import net.crusadergames.bugwars.model.auth.User;
+import net.crusadergames.bugwars.repository.auth.UserRepository;
+import net.crusadergames.bugwars.service.ScriptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/script")
+@PreAuthorize("isAuthenticated()")
+public class ScriptController {
+
+    @Autowired
+    ScriptService scriptService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @PostMapping("/post")
+    public ResponseEntity<Script> postScript(@RequestBody ScriptRequest scriptResponse, Principal principal) {
+        Optional<User> user = userRepository.findByUsername(principal.getName());
+        Script script = scriptService.createNewScript(user.get().getId(), scriptResponse);
+        return new ResponseEntity<>(script, HttpStatus.OK);
+    }
+
+}
