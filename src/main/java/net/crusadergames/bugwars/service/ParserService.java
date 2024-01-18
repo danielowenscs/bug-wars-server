416e98a57caee7e8935d56bf83526a3f0736b1e3
Balderax <balderas.3@osu.edu>
2024-01-18 16:50:54 -0600

    Added ScriptSyntax class in ParserService

--- src/main/java/net/crusadergames/bugwars/service/ParserService.java
+++ src/main/java/net/crusadergames/bugwars/service/ParserService.java
@@ -15,9 +15,6 @@ public class ParserService {
                 "    GOTO DOBBY\n" +
                 ":DOBBY  eat \n";
 
-        final int MIN_EXPRESSIONS = 1;
-        final int MAX_EXPRESSIONS = 2;
-
         List<String> lines = new ArrayList<String>(Arrays.asList(script.split("\\n")));
         Map<String, Boolean> declaredLabels = new HashMap<>();
 
@@ -27,34 +24,32 @@ public class ParserService {
                 continue;
             }
 
-            if (line.contains("#")) {
-                line = line.substring(0, line.indexOf("#"));
-            }
+            line = ScriptSyntax.trimComment(line);
 
             if (line.isEmpty()) {
                 continue;
             }
 
-            final String[] expressions = line.split("\\r+");
+            final String[] expressions = ScriptSyntax.getExpression(line);
             final int expressionsLength = expressions.length;
 
-            if (expressions.length < MIN_EXPRESSIONS) {
+            if (expressions.length < ScriptSyntax.MIN_EXPRESSIONS) {
                 continue;
             }
 
-            if (expressions.length > MAX_EXPRESSIONS) {
+            if (expressions.length > ScriptSyntax.MAX_EXPRESSIONS) {
                 throw new SyntaxException("Too many arguments on line " + i);
             }
 
 
             int keywordIndex = 0;
 
-            if (expressionsLength == MAX_EXPRESSIONS) {
+            if (expressionsLength == ScriptSyntax.MAX_EXPRESSIONS) {
                 keywordIndex = 1;
             }
 
             boolean validLabel = false;
-            if (expressions[0].matches("[:][A-Z][A-Z0-9]*")) {
+            if (ScriptSyntax.isValidLabel(expressions[0])) {
                 if (declaredLabels.containsKey(expressions[0])) {
                     if (declaredLabels.get(expressions[0])) {
                         throw new SyntaxException("Label at line " + " is duplicate");
@@ -62,39 +57,107 @@ public class ParserService {
 
                     throw new SyntaxException("Label at line " + i + " is duplicate");
                 }
-                declaredLabels.put(expressions[0], true);
+                declaredLabels.put(expressions[0].substring(1), true);
                 validLabel = true;
             }
 
-            if (expressionsLength == MAX_EXPRESSIONS && !validLabel) {
+            if (expressionsLength == ScriptSyntax.MAX_EXPRESSIONS && !validLabel) {
                 throw new SyntaxException("Invalid label on line " + i);
             }
 
-            if (keywordIndex == 1 && validLabel) {
+            if (expressionsLength == ScriptSyntax.MIN_EXPRESSIONS && validLabel) {
                 continue;
             }
-
-            /*
-            if (label == null) {
-                throw new SyntaxException("Label ");
+            final String[] parsedKeywords = ScriptSyntax.parseKeyword(expressions[keywordIndex]);
+            if(parsedKeywords == null) {
+                throw new SyntaxException("Invalid Expression: " + expressions[keywordIndex]);
             }
-            if (!label.matches("[A-Z][A-Z0-9]*")) {
-                throw new SyntaxException("Label at line " + i + " should be all Uppercase, Alphanumeric, and start with a Letter ");
+            if(parsedKeywords.length == ScriptSyntax.MAX_EXPRESSIONS) {
+                if(declaredLabels.containsKey(parsedKeywords[1])) {
+
+                }
             }
-            labelsMap.put(label, null);
-
-        }
-        return null;
-
-             */
-
         }
 
-        return false;
+        return true;
     }
 
-    private static class ScriptSyntax {
-        private final static Map<String, Integer> COMMANDS = new HashMap<>(){};
+    private final static class ScriptSyntax {
+        public static final int MIN_EXPRESSIONS = 1;
+        public static final int MAX_EXPRESSIONS = 2;
+        public static final int MIN_BRANCH_CODE = 30;
+        private final static Map<String, Integer> keywordMap = new HashMap<String, Integer>() {{
+            put("noop", 0);
+            put("mov", 10);
+            put("rotr", 11);
+            put("rotl", 12);
+            put("att", 13);
+            put("eat", 14);
+            put("ifEnemy", 30);
+            put("ifAlly", 31);
+            put("ifFood", 32);
+            put("ifEmpty", 33);
+            put("ifWall", 34);
+            put("goto", 35);
+        }};
+
+        public static Boolean isValidKeyword(String keyword) {
+            return keywordMap.containsKey(keyword);
+        }
+
+        public static Integer getOpCode(String keyword) {
+            return keywordMap.get(keyword);
+        }
+
+        public static Boolean isValidLabel(String label) {
+            return label.matches("[:][A-Z][A-Z0-9]*");
+        }
+
+        public static String trimComment(String line) {
+            if (line.contains("#")) {
+                line = line.substring(0, line.indexOf("#"));
+            }
+            return line;
+        }
+
+        public static String[] getExpression(String line) {
+            return line.split("\\r+");
+        }
+
+        public static String[] parseKeyword(String keyword) {
+            /*
+            :LAME goto LAME
+                mov START
+                ifFood EAT
+                att
+                ifWall
+
+             */
+            String[] keywords = keyword.split(" ");
+            if(keywords.length < ScriptSyntax.MIN_EXPRESSIONS || keywords.length > ScriptSyntax.MAX_EXPRESSIONS) {
+                return null;
+            }
+            if(!keywordMap.containsKey(keywords[0])) {
+                return null;
+            }
+            if(keywords.length == ScriptSyntax.MAX_EXPRESSIONS) {
+                int opCode = keywordMap.get(keywords[0]);
+                if(opCode < MIN_BRANCH_CODE) {
+                    return null;
+                }
+                if(!isValidLabel(":" + keywords[1])) {
+                    return null;
+                }
+            }
+            if(keywords.length == ScriptSyntax.MIN_EXPRESSIONS) {
+                int opCode = keywordMap.get(keywords[0]);
+                if(opCode >= MIN_BRANCH_CODE) {
+                    return null;
+                }
+            }
+
+            return keywords;
+        }
 
     }
 
