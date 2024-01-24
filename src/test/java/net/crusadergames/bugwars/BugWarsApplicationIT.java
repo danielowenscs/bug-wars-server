c0716696b4f18ae79cb1fdf0f8d5f4f36d584b21
Maconnell <maconnell.balderas@gmail.com>
2024-01-24 10:26:15 -0600

    Added test for Application Main

---
+++ src/test/java/net/crusadergames/bugwars/BugWarsApplicationIT.java
@@ -0,0 +1,12 @@
+package net.crusadergames.bugwars;
+
+import org.junit.jupiter.api.Test;
+
+class BugWarsApplicationIT {
+
+    @Test
+    public void main() {
+        BugWarsApplication.main(new String[] {});
+    }
+
+}
